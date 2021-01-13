f74e6ce9d8238b259f318f2f84c8fbdb23b5b1cc
Taraxacum <shen_jiamin@outlook.com>
2021-01-14 00:18:16 +0800

    initial commit

---
+++ jupyter-lab/fonts/get_fonts.sh
@@ -0,0 +1,5 @@
+wget https://mirrors.tuna.tsinghua.edu.cn/adobe-fonts/source-han-sans/OTF/SourceHanSansSC.zip && \
+wget https://mirrors.tuna.tsinghua.edu.cn/adobe-fonts/source-han-serif/OTF/SourceHanSerifSC_EL-M.zip && \
+wget https://mirrors.tuna.tsinghua.edu.cn/adobe-fonts/source-han-serif/OTF/SourceHanSerifSC_SB-H.zip && \
+unzip *.zip && \
+mv **/*.otf ./
